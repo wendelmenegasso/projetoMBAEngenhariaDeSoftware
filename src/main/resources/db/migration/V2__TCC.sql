50465dae154e398a9b130ab0e53acad70df9ef27
Wendel <wendel.s.menegasso@gmail.com>
2023-06-17 23:58:24 -0300

    Atualizando versão da base de dados.

---
+++ src/main/resources/db/migration/V2__TCC.sql
@@ -0,0 +1,44 @@
+insert into banco (id, descr) values (1, "Banco do Brasil"),
+(2, "Bradesco"),
+(3, "C6"),
+(4, "Caixa Ecnonomica"),
+(5, "Itau"),
+(6, "Nubaank"),
+(7, "Banco Pan"),
+(8, "Santander"),
+(9, "Outros");
+
+insert into tipoconta (id, descr) values (1, "Conta Corrente"),
+(2, "Conta Poupanca"),
+(3, "Conta Empresarial"),
+(4, "Conta Salario");
+
+insert into repeticao (id, descr) values (1, "diario"),
+(2, "semanal"),
+(3, "quinzenal"),
+(4, "mensal"),
+(5, "bimestral"),
+(6, "trimestral"),
+(7, "quadrimestral"),
+(8, "semestral"),
+(9, "anual"),
+(10, "bianual"),
+(11, "quadrienal");
+
+insert into tipoveiculos (id, descr) values (1, "aviao"),
+(2, "barco"),
+(3, "bicicleta"),
+(4, "carro"),
+(5, "caminhao"),
+(6, "helicoptero"),
+(7, "jetsky"),
+(8, "moto"),
+(9, "onibus");
+
+insert into tipoimovel (id, descr) values (1, "apartamento"),
+(2, "casa"),
+(3, "chacara"),
+(4, "fazenda"),
+(5, "kitnet"),
+(6, "predio");
+
